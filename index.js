e9992d8fd36c87be754e832206e64d91fa7fc8a0
Azir Brooks <azir.brooks@gmail.com>
2020-03-12 08:32:02 +0000

    Add helmet

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const helmet = require('helmet');
 
 const port = process.env.PORT || 3000;
 const session = require('./src/session');
+
+// Load helmet
+app.use(helmet());
 
 // Start session
 app.use(session);
