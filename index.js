e6c8613c3929f743fc3bff4578fc458db6ca3a33
Azir Brooks <azir.brooks@gmail.com>
2020-03-13 16:37:28 +0000

    Move routes to ./src/routes

--- index.js
+++ index.js
@@ -3,40 +3,17 @@ const app = express();
 const http = require('http').createServer(app);
 const helmet = require('helmet');
 const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
+const routes = require('./src/routes');
 
 const port = process.env.PORT || 3000;
 const session = require('./src/session');
 const cors = require('./src/cors');
-const jsonParser = bodyParser.json();
-const User = require('./src/User');
 
 /**  App middlewares */
 app.use(helmet());
 app.use(cors);
 app.use(session);
-
-/** App routes */
-app.get('/', function(req, res) {
-  let isAuthenticated = false;
-
-  if (req.session.user !== undefined) {
-    isAuthenticated = true;
-  }
-
-  res.json({ isAuthenticated: isAuthenticated });
-});
-
-app.post('/', jsonParser, function(req, res) {
-  let isAuthenticated = false;
-
-  if (req.body.userName) {
-    req.session.user = new User(req.body.userName);
-    isAuthenticated = true;
-  }
-
-  res.json({ isAuthenticated: isAuthenticated });
-});
+app.use('/', routes);
 
 /** Socket events */
 io.origins(process.env.CORS_ORIGIN);
