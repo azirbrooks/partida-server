5f9b0d06aec8bf7c0423c3a6f8d3b41cd4d97959
Doru <doru.dev.now@gmail.com>
2020-03-21 11:16:34 +0100

    test it out

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const helmet = require('helmet');
-const io = require('socket.io')(http);
-const routes = require('./src/routes');
+const http = require("http").createServer(app);
+const helmet = require("helmet");
+const io = require("socket.io")(http);
+const routes = require("./src/routes");
 
 const port = process.env.PORT || 3000;
-const session = require('./src/session');
-const cors = require('./src/cors');
+const session = require("./src/session");
+const cors = require("./src/cors");
 
 /**  App middlewares */
 app.use(helmet());
 app.use(cors);
 app.use(session);
-app.use('/', routes);
+app.use("/", routes);
 
 /** Socket events */
 io.origins(process.env.CORS_ORIGIN);
-io.on('connection', function(socket) {
+io.on("connection", function(socket) {
+  console.log(socket);
   // socket connected
 });
 
 /** Start http server */
 http.listen(port, () => {
-  console.log('Server listening at port %d', port);
+  console.log("Server listening at port %d", port);
 });
